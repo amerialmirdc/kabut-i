bec8fbb897e467262310188ad423979d01db8f6c
unknown <Administrator@BAKALdomain.loc>
2025-02-17 11:20:04 +0800

    redirect / to /login

--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
+'use client'
+import { useEffect } from "react"
+import { useRouter } from 'next/navigation'
 
-export default function Home() {
-  return (
-    <div>Hello kabuti</div>
-  );
+export default function Home(){
+  const router = useRouter()
+
+  useEffect(()=>{
+    router.push('/login')
+  }, [])
+
+  return(
+    <>
+      <h1>Dashboard</h1>
+    </>
+  )
 }
